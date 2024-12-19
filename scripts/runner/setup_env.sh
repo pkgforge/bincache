9b7c0774e3057d0ad435ee64518dabadf01de765
Ajam <AjamX101@gmail.com>
2024-12-19 10:38:03 +0545

    saner env

---
+++ scripts/runner/setup_env.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+##
+# source <(curl -qfsSL "https://raw.githubusercontent.com/pkgforge/bincache/refs/heads/main/scripts/runner/setup_env.sh")
+##
+
+#-------------------------------------------------------#
+USER="$(whoami)" && export USER="${USER}"
+HOME="$(getent passwd ${USER} | cut -d: -f6)" && export HOME="${HOME}"
+if [ -z "${SYSTMP+x}" ] || [ -z "${SYSTMP##*[[:space:]]}" ]; then
+ SYSTMP="$(dirname $(mktemp -u))" && export SYSTMP="${SYSTMP}"
+fi
+export PATH="${HOME}/bin:${HOME}/.cargo/bin:${HOME}/.cargo/env:${HOME}/.go/bin:${HOME}/go/bin:${HOME}/.local/bin:${HOME}/miniconda3/bin:${HOME}/miniconda3/condabin:/usr/local/zig:/usr/local/zig/lib:/usr/local/zig/lib/include:/usr/local/musl/bin:/usr/local/musl/lib:/usr/local/musl/include:${PATH}"
+PATH="$(echo "${PATH}" | awk 'BEGIN{RS=":";ORS=":"}{gsub(/\n/,"");if(!a[$0]++)print}' | sed 's/:*$//')" ; export PATH
+PKG_REPO="bincache"
+TMPDIRS="mktemp -d --tmpdir=${SYSTMP}/pkgforge XXXXXXX_SBUILD"
+USER_AGENT="$(curl -qfsSL 'https://pub.ajam.dev/repos/Azathothas/Wordlists/Misc/User-Agents/ua_chrome_macos_latest.txt')"
+export HOST_TRIPLET PKG_REPO SYSTMP TMPDIRS USER_AGENT
+if [[ "${KEEP_PREVIOUS}" != "YES" ]]; then
+ rm -rf "${SYSTMP}/pkgforge"
+fi
+mkdir -pv "${SYSTMP}/pkgforge"
+export GIT_TERMINAL_PROMPT="0"
+export GIT_ASKPASS="/bin/echo"
+EGET_TIMEOUT="timeout -k 1m 2m" && export EGET_TIMEOUT="${EGET_TIMEOUT}"
+sudo groupadd docker 2>/dev/null ; sudo usermod -aG docker "${USER}" 2>/dev/null
+if ! sudo systemctl is-active --quiet docker; then
+ sudo service docker restart >/dev/null 2>&1 ; sleep 10
+fi
+sudo systemctl status "docker.service" --no-pager
+#Nix
+source "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
+#sg docker newgrp "$(id -gn)"
+cd "${HOME}" ; clear
+##Sanity Checks
+for DEP_CMD in eget gh glab oras rclone soar; do
+ case "$(command -v "${DEP_CMD}" 2>/dev/null)" in
+     "") echo -e "\n[✗] FATAL: ${DEP_CMD} is NOT INSTALLED\n"
+ esac
+done
+if [[ ! -n "${GITHUB_TOKEN}" ]]; then
+ echo -e "\n[-] GITHUB_TOKEN is NOT Exported"
+ echo -e "Export it to Use GH\n"
+fi
+if [[ ! -n "${GHCR_TOKEN}" ]]; then
+ echo -e "\n[-] GHCR_TOKEN is NOT Exported"
+ echo -e "Export it to avoid ghcr\n"
+fi
+if [[ ! -n "${GITLAB_TOKEN}" ]]; then
+ echo -e "\n[-] GITLAB_TOKEN is NOT Exported"
+ echo -e "Export it to Use GH\n"
+fi
+#-------------------------------------------------------#
+unset DOCKER_HOST
+history -c 2>/dev/null ; rm -rf "${HOME}/.bash_history" ; pushd "$(mktemp -d)" >/dev/null 2>&1
+#-------------------------------------------------------#
