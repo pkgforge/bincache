ed8dc9e3838555f877e7f0eb88dc50783457c9a5
Azathothas <AjamX101@gmail.com>
2025-01-21 22:36:49 +0545

    exclude cached

--- scripts/runner/builder.sh
+++ scripts/runner/builder.sh
@@ -15,7 +15,7 @@
 sbuild_builder()
  {
   ##Version
-   SBB_VERSION="0.1.9" && echo -e "[+] SBUILD Builder Version: ${SBB_VERSION}" ; unset SBB_VERSION
+   SBB_VERSION="0.2.0" && echo -e "[+] SBUILD Builder Version: ${SBB_VERSION}" ; unset SBB_VERSION
   ##Enable Debug 
    if [ "${DEBUG}" = "1" ] || [ "${DEBUG}" = "ON" ]; then
       set -x
@@ -194,7 +194,20 @@ sbuild_builder()
     fi
    else
    #Get URlS
-    curl -qfsSL "https://raw.githubusercontent.com/pkgforge/bincache/refs/heads/main/SBUILD_LIST.json" -o "${SYSTMP}/pkgforge/SBUILD_LIST.json"
+    if [[ "${EXCLUDE_CACHED}" == "YES" ]]; then
+     curl -qfsSL "https://raw.githubusercontent.com/pkgforge/metadata/refs/heads/main/soarpkgs/data/DIFF_bincache_$(uname -m)-$(uname -s).json" -o "${SYSTMP}/pkgforge/DIFF.json"
+     curl -qfsSL "https://raw.githubusercontent.com/pkgforge/bincache/refs/heads/main/SBUILD_LIST.json" -o "${SYSTMP}/pkgforge/SBUILD_LIST.json"
+     jq -n --slurpfile a "${SYSTMP}/pkgforge/DIFF.json" --slurpfile b "${SYSTMP}/pkgforge/SBUILD_LIST.json" \
+    '
+     [$b[] | .[] as $objB | 
+       ($a[] | .[] | .build_script | split("/") | .[-3:] | join("/")) as $a_last3 |
+       ($objB.build_script | split("/") | .[-3:] | join("/")) as $b_last3 |
+       if $a_last3 == $b_last3 or $objB.rebuild == true then $objB else empty end
+     ] | unique_by(.ghcr_pkg) | sort_by(.pkg_family)
+    ' | jq . > "${SYSTMP}/pkgforge/SBUILD_LIST.json"
+    else
+     curl -qfsSL "https://raw.githubusercontent.com/pkgforge/bincache/refs/heads/main/SBUILD_LIST.json" -o "${SYSTMP}/pkgforge/SBUILD_LIST.json"
+    fi
     jq -r '.[] | select(._disabled == false) | .build_script' "${SYSTMP}/pkgforge/SBUILD_LIST.json" | sort -u -o "${SYSTMP}/pkgforge/SBUILD_URLS"
     sed 's|https://github.com/pkgforge/soarpkgs/blob/main/binaries|https://raw.githubusercontent.com/pkgforge/soarpkgs/refs/heads/main/binaries|g' -i "${SYSTMP}/pkgforge/SBUILD_URLS"
    fi
