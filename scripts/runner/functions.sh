ec2b05b1102f64c92e16d10536bd5ab7083cd95a
Azathothas <AjamX101@gmail.com>
2025-01-29 22:04:25 +0545

    fix

--- scripts/runner/functions.sh
+++ scripts/runner/functions.sh
@@ -31,8 +31,8 @@ setup_env()
  fi
  BUILD_DIR="$(mktemp -d --tmpdir=${SYSTMP}/pkgforge XXXXXXX_$(basename "${INPUT_SBUILD}" | tr -d 'X'))"
  SBUILD_OUTDIR="${BUILD_DIR}/SBUILD_OUTDIR"
- SBUILD_TMPDIR="${SBUILD_OUTDIR}/SBUILD_TEMP/tmp"
- mkdir -p "${SBUILD_TMPDIR}"
+ SBUILD_TMPDIR="${SBUILD_OUTDIR}/SBUILD_TEMP"
+ mkdir -p "${SBUILD_TMPDIR}/tmp"
  export BUILD_DIR INPUT_SBUILD SBUILD_OUTDIR SBUILD_TMPDIR
  #echo -e "\n[+] Building ["$(echo "${RECIPE}" | awk -F'/' '{print $(NF-1) "/" $NF}')"] (${INPUT_SBUILD}) --> ${SBUILD_OUTDIR} [$(TZ='UTC' date +'%A, %Y-%m-%d (%I:%M:%S %p)') UTC]\n"
  echo -e "\n[+] Building (${SBUILD_SCRIPT_BLOB:-${RECIPE}}) --> ${SBUILD_OUTDIR} [$(TZ='UTC' date +'%A, %Y-%m-%d (%I:%M:%S %p)') UTC]\n"
